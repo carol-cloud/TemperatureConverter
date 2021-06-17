0b1848f492169e68aa7f2765e71abff5bb6b7121
Ana Carolina <karols.15@hotmail.com>
2021-06-16 23:01:10 -0300

    Adicionar arquivos de projeto.

---
+++ TemperatureConverter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius, kelvin, fahrenheit;
+
+            Console.WriteLine("Conversor de Temperaturas");
+            Console.WriteLine("Insira a temperatura em Celsius: ");
+            celsius = double.Parse(Console.ReadLine());
+
+            kelvin = celsius + 273.15;
+            fahrenheit = celsius * 1.8 + 32;
+
+            Console.WriteLine("Graus em Celsius: " + celsius);
+            Console.WriteLine("Graus em Kelvin: " + kelvin);
+            Console.WriteLine("Graus em Fahrenheit: " + fahrenheit);
+            Console.ReadKey();
+
+        }
+    }
+}
